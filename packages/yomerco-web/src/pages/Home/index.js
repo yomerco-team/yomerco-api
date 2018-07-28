8e77a829a1a9773c1172458d95637f395196cc84
Carlos Navarro <carlosarturo28@gmail.com>
2018-07-28 14:20:16 -0500

    adding new project

---
+++ packages/yomerco-web/src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from '../../assets/logo.svg';
+import { Header, Container } from './styles';
+
+class Home extends Component {
+  render() {
+    return (
+        <Container>
+            <Header>
+                <img src={logo} alt="YoMerco-logo-mercado-online"/>
+            </Header>
+        </Container>
+    );
+  }
+}
+
+export default Home;
