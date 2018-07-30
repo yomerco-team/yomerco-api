cb8baa28b8174f91533dc6e5c5a6451f58c6de49
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2018-07-29 20:06:19 -0500

    feat(web): 13 - adding lint to web

--- packages/yomerco-web/src/index.js
+++ packages/yomerco-web/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Home from './pages/Home';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Home from './pages/Home'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<Home />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<Home />, document.getElementById('root'))
+registerServiceWorker()
--- packages/yomerco-web/src/pages/Home/index.js
+++ packages/yomerco-web/src/pages/Home/index.js
@@ -1,17 +1,17 @@
-import React, { Component } from 'react';
-import logo from '../../assets/logo.svg';
-import { Header, Container } from './styles';
+import React, { Component } from 'react'
+import logo from '../../assets/logo.svg'
+import { Header, Container } from './styles'
 
 class Home extends Component {
-  render() {
+  render () {
     return (
-        <Container>
-            <Header>
-                <img src={logo} alt="YoMerco-logo-mercado-online"/>
-            </Header>
-        </Container>
-    );
+      <Container>
+        <Header>
+          <img src={logo} alt='YoMerco-logo-mercado-online' />
+        </Header>
+      </Container>
+    )
   }
 }
 
-export default Home;
+export default Home
