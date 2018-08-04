ec06344404da775b18b54a678aee2817bdcaa7d3
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2018-08-04 13:54:37 -0500

    feat(api): 27 - creating a deleting user from firebase with tests

--- packages/yomerco-api/server/services/firebase.js
+++ packages/yomerco-api/server/services/firebase.js
@@ -1,24 +1,25 @@
 const firebaseAdmin = require('firebase-admin')
 const serviceAccountKey = require('./serviceAccountKey')
 
-class FirebaBase {
+class Firebase {
   constructor () {
     this.admin = firebaseAdmin.initializeApp({
       credential: firebaseAdmin.credential.cert(serviceAccountKey),
       databaseURL: process.env.DATA_BASE_URL_FIRE_BASE
     })
+
+    this.auth = firebaseAdmin.auth()
   }
 
-  auth = firebaseAdmin.auth()
-
   /**
-  * funcion encargada de actualizar el email del usuario
+  * Function to update the email for a user
   *
-  * @param {String} uid
-  * @param String} email
-  * @returns {Promise} objeto con la info
+  * @param {String} uid identifier in firebase
+  * @param {String} email email to update
+  * @returns {Promise} Object with the information
+  * @memberof Firebase
   */
-  updateEmail = async (uid, email) => {
+  async updateEmail (uid, email) {
     // Actualizo el usuario
     try {
       this.auth.updateUser(uid, { email })
@@ -32,26 +33,55 @@ class FirebaBase {
   }
 
   /**
-  * Funcion encargada de crear un usuario en firebase
+  * Function to create a user in firebase
   *
-  * @param {String} email email del usuario
-  * @param {String} password clave del usuario
-  * @returns {Promise} objeto con la info del usuario creado en firebase
+  * @param {String} email email to the user
+  * @param {String} password password to the user
+  * @returns {Promise} Object that have the info about the user in firebase
+  * @memberof Firebase
   */
- createUser = async (email, password) => {
-   const objToUser = { email, password }
+  async createUser (email, password) {
+    const objToUser = { email, password }
 
-   let userRecord = null
-   try {
-     userRecord = await this.auth.createUser(objToUser)
-   } catch (error) {
-     throw error
-   }
+    let userRecord = null
+    try {
+      userRecord = await this.auth.createUser(objToUser)
+      userRecord = userRecord.toJSON()
+    } catch (error) {
+      throw error
+    }
 
-   return userRecord
- }
+    return userRecord
+  }
+
+  /**
+   * Fucntion to delete a user in firebase
+   *
+   * @param {*} email email from user
+   * @returns {Promise} a resolved promise
+   * @memberof Firebase
+   */
+  async deleteUser (email) {
+    // Get the user by email parameter
+    let userRecord = null
+    try {
+      userRecord = await this.auth.getUserByEmail(email)
+    } catch (error) {
+      throw error
+    }
+
+    userRecord = userRecord.toJSON()
+
+    try {
+      await this.auth.deleteUser(userRecord.uid)
+    } catch (error) {
+      throw error
+    }
+
+    return Promise.resolve()
+  }
 }
 
-const firebaInstance = new FirebaBase()
+const firebaInstance = new Firebase()
 
-module.exports = { firebaInstance }
+module.exports = firebaInstance
---
+++ packages/yomerco-api/unit-test/models/my-user.test.js
@@ -0,0 +1,28 @@
+/* eslint-env jest */
+process.env.NODE_ENV = 'test'
+const faker = require('faker')
+
+test('create a user in firebase', async () => {
+  expect.assertions(1)
+  const app = require('../../server/server')
+  const { MyUser } = app.models
+  const randomEmail = faker.internet.email().toLowerCase()
+
+  // create the user for the test
+  let userRow = null
+  try {
+    userRow = await MyUser.createUserInFirebase(randomEmail, '123456')
+  } catch (error) {
+    throw error
+  }
+
+  // delete the user for the test
+  let response = null
+  try {
+    response = await MyUser.deleteUserInFirebase(userRow.email)
+  } catch (error) {
+    throw error
+  }
+
+  expect(response.count).toBe(1)
+})
