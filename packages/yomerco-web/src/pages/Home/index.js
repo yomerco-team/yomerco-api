1d1bf01f3aaa93e39bf1a169dcc5063f2708f68b
Carlos Navarro <carlosarturo28@gmail.com>
2018-08-06 20:50:54 -0500

    feat(yomerco-web): 32 - add all the components to the home screen

--- packages/yomerco-web/src/pages/Home/index.js
+++ packages/yomerco-web/src/pages/Home/index.js
@@ -1,8 +1,26 @@
 import React, { Component } from 'react'
 import Header from '../../components/Header'
 import tienda from '../../assets/tienda.svg'
+import logoAlt from '../../assets/logo-alt.svg'
 import ProductCard from '../../components/ProductCard'
-import { Container, Content, Section, Image, Title, ValueProposal, Subtitle, SectionTitle } from './styles'
+import {
+  Container,
+  Content, Section,
+  Image,
+  Title,
+  ValueProposal,
+  Subtitle,
+  SectionTitle,
+  SeeAll,
+  CategoryContainer,
+  CategoryContent,
+  CategoryText,
+  CategoryImage,
+  FooterContent,
+  FooterImage,
+  FooterDescription,
+  Copyright
+} from './styles'
 
 class Home extends Component {
   render () {
@@ -26,9 +44,52 @@ class Home extends Component {
           <ProductCard />
           <ProductCard />
           <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
+          <SeeAll>
+            Ver todos los productos
+          </SeeAll>
+          <SectionTitle>
+            Otras categorías
+          </SectionTitle>
+          <CategoryContent>
+            <CategoryContainer>
+              <CategoryImage src='https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/201706/15/00202002800106____1__600x600.jpg' alt='YoMerco-categoria-verduras' />
+              <CategoryText>
+              Verduras
+              </CategoryText>
+            </CategoryContainer>
+            <CategoryContainer>
+              <CategoryImage src='https://sgfm.elcorteingles.es/SGFM/dctm/MEDIA03/201703/15/00118104500022____2__600x600.jpg' alt='YoMerco-categoria-frutas' />
+              <CategoryText>
+              Frutas
+              </CategoryText>
+            </CategoryContainer>
+            <CategoryContainer>
+              <CategoryImage src='https://s3-us-west-2.amazonaws.com/mercadoni/live/e6a6f9c379333b3155d5ab425467c7c04ff8b05c/medium_e6a6f9c379333b3155d5ab425467c7c04ff8b05c.jpg' alt='YoMerco-categoria-carnes' />
+              <CategoryText>
+              Carnes
+              </CategoryText>
+            </CategoryContainer>
+            <CategoryContainer>
+              <CategoryImage src='https://ctsecure.saveonfoods.com/legacy/productimagesroot/DJ/1/1306431.jpg' alt='YoMerco-categoria-variados' />
+              <CategoryText>
+              Variados
+              </CategoryText>
+            </CategoryContainer>
+          </CategoryContent>
+          <FooterContent>
+            <FooterImage src={logoAlt} alt='alternative-logo-yomerco' />
+            <FooterDescription>
+            Sabemos que tus días son duros. Trabajar, estudiar, hacer pereza… Por eso
+             hicimos Yo Merco, para que puedas realizar tu mercado sin salir de casa.
+            </FooterDescription>
+            <FooterDescription>
+            Además, apoyamos al agro nacional vendiendo
+            productos <b>producidos por campesinos colombianos.</b>
+            </FooterDescription>
+          </FooterContent>
+          <Copyright>
+            <b>yomerco.co</b> es una marca registrada - 2018.
+          </Copyright>
         </Container>
       </Content>
     )
--- packages/yomerco-web/src/pages/Home/styles.js
+++ packages/yomerco-web/src/pages/Home/styles.js
@@ -22,7 +22,7 @@ border-bottom: 1px solid #d5d5d5;
 
 export const Image = styled.img`
 height: auto;
-width: 160px;
+width: 140px;
 `
 
 export const ValueProposal = styled.div`
@@ -49,4 +49,73 @@ font-size: 22px;
 color: #585858;
 padding: 12px 10px;
 border-bottom: 4px solid #d5d5d5;
+`
+
+export const SeeAll = styled.button`
+font-family: 'circular-book';
+color: #0074B8;
+font-size: 16px;
+background-color: white;
+border: 1px solid #d5d5d5;
+border-radius: 4px;
+width: 94%;
+padding: 8px 0;
+margin: 22px 3%;
+`
+
+export const CategoryContent = styled.div`
+display: flex;
+flex-wrap: wrap;
+padding: 12px 18px 24px 18px;
+border-bottom: 1px solid #d5d5d5;
+justify-content: space-between;
+`
+
+export const CategoryContainer = styled.a`
+margin: 16px 10px 10px 10px;
+display: flex;
+flex-direction: column;
+`
+
+export const CategoryText = styled.p`
+font-family: 'circular-medium';
+font-size: 18px;
+margin-top: 12px;
+color: #585858;
+text-align: center;
+`
+export const CategoryImage = styled.img`
+height: 84px;
+width: 84px;
+`
+export const FooterContent = styled.div`
+display: flex;
+padding: 0 18px;
+justify-content: center;
+flex-direction: column;
+`
+
+export const FooterImage = styled.img`
+height: auto;
+width: 130px;
+margin: 20px auto;
+`
+
+export const FooterDescription = styled.p`
+font-family: 'circular-book';
+font-size: 14px;
+margin-bottom:10px;
+color: #585858;
+text-align: center;
+`
+
+export const Copyright = styled.p`
+width: 100%;
+margin-top: 12px;
+padding: 18px 0;
+text-align: center;
+font-family: 'circular-book';
+font-size: 14px;
+color: white;
+background-color: #409640;
 `
