7bae40983d0e43908a207a4887251ce9a7c26551
Carlos Navarro <carlosarturo28@gmail.com>
2018-08-07 09:43:49 -0500

    feat(yomerco-web): 34 - adding info links to the drawer

--- packages/yomerco-web/src/components/Drawer/index.js
+++ packages/yomerco-web/src/components/Drawer/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-import { DrawerContainer, DrawerComp, DrawerIcon } from './styles'
+import CategorySection from '../CategorySection'
+import { DrawerContainer, DrawerComp, DrawerIcon, DrawerLink, LinkContainer } from './styles'
 
 class Drawer extends Component {
      state = {
        openLeft: false,
        openRight: false,
-       width: '70%'
+       width: '84%'
      };
 
      render () {
@@ -15,8 +16,18 @@ class Drawer extends Component {
              fadeOut
              width={this.state.width}
              onChange={open => this.setState({ openLeft: open })}
-             noTouchOpen
-             noTouchClose />
+             noTouchOpen>
+             <LinkContainer>
+             <DrawerLink>Iniciar sesión o registrarse</DrawerLink>
+             </LinkContainer>
+             <CategorySection drawer />
+             <LinkContainer>
+             <DrawerLink>Contáctanos</DrawerLink>
+             </LinkContainer>
+             <LinkContainer>
+             <DrawerLink>FAQs</DrawerLink>
+             </LinkContainer>
+             </DrawerComp>
            <a onClick={() => this.setState({ openLeft: !this.state.openLeft, openRight: false })}>
              <DrawerIcon />
            </a>
