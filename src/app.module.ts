cb46f4af6dc639d0327626559f79f44d9240490f
Cristian David Ippolito <cristiandavidippolito@gmail.com>
2020-11-01 12:03:24 -0500

    feat: adding config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
+import * as path from 'path';
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import appConfig from './config/app.config';
+import appConfigSchema from './config/app.config.schema';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const NODE_ENV = process.env.NODE_ENV || 'local';
+const envPath = path.resolve(__dirname, `../.env.${NODE_ENV}`);
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: envPath,
+      load: [appConfig],
+      validationSchema: appConfigSchema
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
---
+++ src/config/app.config.schema.ts
@@ -0,0 +1,11 @@
+import * as Joi from 'joi';
+
+export default Joi.object({
+  /* DATABASE INFORMATION */
+  DATABASE_CLIENT: Joi.required(),
+  DATABASE_HOST: Joi.required(),
+  DATABASE_PORT: Joi.number().default(5432),
+  DATABASE_USER: Joi.required(),
+  DATABASE_PASSWORD: Joi.required(),
+  DATABASE_NAME: Joi.required()
+});
