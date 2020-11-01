61b522ad32f22bd732a452005998bbde9b50820d
Cristian David Ippolito <cristiandavidippolito@gmail.com>
2020-11-01 11:44:59 -0500

    feat(cz): config husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,29 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
+    project: './${workspaceFolder}/tsconfig.json',
+    sourceType: 'module'
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'prettier',
-    'prettier/@typescript-eslint',
+    'prettier/@typescript-eslint'
   ],
   root: true,
   env: {
     node: true,
-    jest: true,
+    jest: true
   },
   rules: {
+    indent: ['error', 2],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'comma-dangle': ['error', 'never'],
-    'no-console': ['error', { allow: ['error'] }]
+    'no-console': ['error', { allow: ['warn', 'error'] }]
   }
 };
