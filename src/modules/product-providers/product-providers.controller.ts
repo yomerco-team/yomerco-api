8d32d76f6f0b3c78a71c6a497d73a386a9adeef4
Cristian David Ippolito Carrillo <cristiandavidippolito@hotmail.com>
2021-01-11 21:58:20 -0500

    feat: product provider crud

--- src/modules/product-providers/product-providers.controller.ts
+++ src/modules/product-providers/product-providers.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 import { ProductProvidersService } from './product-providers.service';
 
@@ -7,33 +8,29 @@ import { FindAllInput } from './dto/find-all-input.dto';
 import { FindOneInput } from './dto/find-one-input.dto';
 import { UpdateInput } from './dto/update-input.dto';
 
+@ApiTags('references')
+@UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
 @Controller('product-providers')
 export class ProductProvidersController {
   constructor(private readonly service: ProductProvidersService) {}
 
   @Post()
-  public create(@Body() createInput: CreateInput) {
-    return {
-      message: 'ok'
-    };
+  public create(@Body() createInput: CreateInput): Promise<any> {
+    return this.service.create(createInput);
   }
 
   @Get()
-  public list(@Query() listInput: FindAllInput) {
-    return [];
+  public list(@Query() listInput: FindAllInput): Promise<any> {
+    return this.service.list(listInput);
   }
 
   @Patch(':id')
-  public update(@Param() findOneInput: FindOneInput, @Body() updateBodyInput: UpdateInput) {
-    return {
-      message: 'ok'
-    };
+  public update(@Param() findOneInput: FindOneInput, @Body() updateBodyInput: UpdateInput): Promise<any> {
+    return this.service.update(findOneInput, updateBodyInput);
   }
 
   @Delete(':id')
-  public delete(@Param() findOneInput: FindOneInput) {
-    return {
-      message: 'ok'
-    };
+  public delete(@Param() findOneInput: FindOneInput): Promise<any> {
+    return this.service.delete(findOneInput);
   }
 }
--- src/modules/product-providers/product-providers.service.ts
+++ src/modules/product-providers/product-providers.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
 import { ProductProvider } from './product-provider.entity';
+
+import { CreateInput } from './dto/create-input.dto';
+import { FindAllInput } from './dto/find-all-input.dto';
+import { FindOneInput } from './dto/find-one-input.dto';
+import { UpdateInput } from './dto/update-input.dto';
 
 @Injectable()
 export class ProductProvidersService {
@@ -10,4 +15,97 @@ export class ProductProvidersService {
     @InjectRepository(ProductProvider)
     private readonly productProviderRepository: Repository<ProductProvider>
   ){}
+
+  /**
+   *
+   *
+   * @param {CreateInput} createInput
+   * @return {*}  {Promise<ProductProvider>}
+   * @memberof ProductProvidersService
+   */
+  public async create(createInput: CreateInput): Promise<ProductProvider> {
+    const created = this.productProviderRepository.create({
+      address: createInput.address,
+      document: createInput.document,
+      email: createInput.email,
+      nit: createInput.nit,
+      phone: createInput.phone
+    });
+
+    const saved = await this.productProviderRepository.save(created);
+
+    return saved;
+  }
+
+  /**
+   *
+   *
+   * @param {FindAllInput} listInput
+   * @return {*}  {Promise<ProductProvider[]>}
+   * @memberof ProductProvidersService
+   */
+  public async list(listInput: FindAllInput): Promise<ProductProvider[]> {
+    const { limit = 0, offset = 0 } = listInput;
+
+    const items = await this.productProviderRepository.createQueryBuilder('pp')
+      .limit(limit || undefined)
+      .skip(offset)
+      .getMany();
+
+    return items;
+  }
+
+  /**
+   *
+   *
+   * @param {FindOneInput} findOneInput
+   * @return {*}  {(Promise<ProductProvider | null>)}
+   * @memberof ProductProvidersService
+   */
+  public async findOne(findOneInput: FindOneInput): Promise<ProductProvider | null> {
+    const { id } = findOneInput;
+
+    const item = await this.productProviderRepository.createQueryBuilder('pp')
+      .where('pp.id = :id', { id })
+      .getOne();
+
+    return item || null;
+  }
+
+  /**
+   *
+   *
+   * @param {FindOneInput} findOneInput
+   * @param {UpdateInput} updateBodyInput
+   * @return {*}  {Promise<ProductProvider>}
+   * @memberof ProductProvidersService
+   */
+  public async update(findOneInput: FindOneInput, updateBodyInput: UpdateInput): Promise<ProductProvider> {
+    const existing = await this.findOne(findOneInput);
+
+    if (!existing) {
+      throw new NotFoundException(`can't get the product provider with id ${findOneInput.id}.`);
+    }
+
+    const preloaded = await this.productProviderRepository.preload({
+      id: existing.id,
+      ...updateBodyInput
+    });
+
+    const saved = await this.productProviderRepository.save(preloaded);
+
+    return saved;
+  }
+
+  public async delete(findOneInput: FindOneInput): Promise<ProductProvider> {
+    const item = await this.findOne(findOneInput);
+
+    if (!item) {
+      throw new NotFoundException(`can't get the product provider with id ${findOneInput.id}.`);
+    }
+
+    const deleted = await this.productProviderRepository.remove(item);
+
+    return deleted;
+  }
 }
