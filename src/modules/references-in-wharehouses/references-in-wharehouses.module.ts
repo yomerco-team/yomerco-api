c2a2fecc8f2efecd40c596e158012dd74e4ad46a
Cristian David Ippolito Carrillo <cristiandavidippolito@hotmail.com>
2021-01-14 22:44:01 -0500

    feat: importing entity in references in whrehouses

--- src/modules/references-in-wharehouses/references-in-wharehouses.module.ts
+++ src/modules/references-in-wharehouses/references-in-wharehouses.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ReferenceInWharehouse } from './reference-in-wharehouse.entity';
 import { ReferencesInWharehousesService } from './references-in-wharehouses.service';
 
 @Module({
-  providers: [ReferencesInWharehousesService]
+  imports: [TypeOrmModule.forFeature([ReferenceInWharehouse])],
+  providers: [ReferencesInWharehousesService],
+  exports: [ReferencesInWharehousesService]
 })
 export class ReferencesInWharehousesModule {}
